03cc1dde17afc36a8b1de9fffe5d633019a1be15
Bharat Saini <bharat.saini@bcstechnology.com.au>
2024-04-26 09:40:43 +0530

    create a simple go web app

---
+++ myapp.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello, Inc42 Media")
+}
+
+func main() {
+	// Register a handler for the root URL path "/"
+	http.HandleFunc("/", handler)
+
+	// Start the web server on port 8080
+	fmt.Println("Server is running on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
